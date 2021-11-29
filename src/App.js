6794099a0cc0bf9f92ed07c55b71fb9b4471300d
istiak.shanto <55758239+istiak2426@users.noreply.github.com>
2021-11-29 15:38:16 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import Home2 from "./pages/home/Home2";
+// import Profile from "./pages/profile/Profile";
+
+import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+
+
+
+
+function App() {
+  return (
+  
+  
+  <Login />
+    
+  );
+}
+
+export default App;
